79cb8e4530ca1334dcaa343fe44208c1b5df83ce
Deepak Hazarika <deepchin@users.noreply.github.com>
2020-09-20 14:13:21 +0530

    updated init file

--- albumentations/__init__.py
+++ albumentations/__init__.py
@@ -5,5 +5,12 @@ __version__ = "0.4.6"
 from .core.composition import *
 from .core.transforms_interface import *
 from .core.serialization import *
-from .augmentations import *
+#from .augmentations import *
 from .imgaug.transforms import *
+
+# Common classes
+from .augmentations.keypoints_utils import *
+from .augmentations.bbox_utils import *
+from .augmentations.functional import *
+from .augmentations.transforms import *
+
