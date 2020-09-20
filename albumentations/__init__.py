c970a1dd052c8fe7bd8ea6c5d195c35d300483a5
Deepak Hazarika <deepchin@users.noreply.github.com>
2020-09-20 13:55:23 +0530

    added albumenation from https://github.com/albumentations-team/albumentations

---
+++ albumentations/__init__.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+__version__ = "0.4.6"
+
+from .core.composition import *
+from .core.transforms_interface import *
+from .core.serialization import *
+from .augmentations import *
+from .imgaug.transforms import *
