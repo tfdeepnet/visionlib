15fb712e3208a63d62aa8ad593ce381c6ffd9597
Deepak Hazarika <deepchin@users.noreply.github.com>
2020-09-21 13:57:59 +0530

    updated setup file

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup,  find_namespace_packages
 setup(
     name='visionlib',
     version='1.0.0',
-    packages=find_namespace_packages(include=["albumentations.*","models.*","utils.*"]),
+    packages=["albumentations.*","models.*","utils.*"],
     url='git+https://github.com/tfdeepnet/visionlib.git',
     license='MIT',
     author='Deepak',
