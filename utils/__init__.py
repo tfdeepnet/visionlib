76035d31e57af73d015d2f7d55dfaa575fd41c9d
Deepak Hazarika <deepchin@users.noreply.github.com>
2020-09-27 02:45:12 +0530

    add loadalbumentationdata file

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,3 +4,4 @@ from .loaddata import loaddata
 from .torchsummarywithrf import summary
 from .loadalbumentationdata import loadalbumentationdata
 from .grad_cam import GradCam
+from .grad_cam import preprocess_image
--- utils/loadalbumentationdata.py
+++ utils/loadalbumentationdata.py
@@ -72,7 +72,7 @@ def loadalbumentationdata(datafolder , batch_size ):
     albumentations_transform_train = A.Compose([
         A.HueSaturationValue(hue_shift_limit=20, sat_shift_limit=20, val_shift_limit=10, p=0.3),
         A.ShiftScaleRotate(shift_limit=0.1, scale_limit=0.9, rotate_limit=10, p=0.3),
-        #A.HorizontalFlip(),
+        A.HorizontalFlip(),
         A.Normalize(
             mean=[0.5, 0.5, 0.5],
             std=[0.5, 0.5, 0.5],
