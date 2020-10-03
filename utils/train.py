80ee89b58ef3302e0eb306553ada39a5f93a1cb1
Deepak Hazarika <deepchin@users.noreply.github.com>
2020-10-03 13:09:37 +0530

    updated train and test file to store accuracy and loss

--- utils/train.py
+++ utils/train.py
@@ -39,7 +39,7 @@ def train(epoch,  trainloader, optimizer, net, criterion, num_bucket,device):
         running_loss += loss.item()
         if idx % (num_bucket) == 0:    # print every 2000 mini-batches
              print('[%d, %5d] loss: %.3f' %
-                   (epoch + 1, i + 1, running_loss / (num_bucket)))
+                   (epoch + 1, idx + 1, running_loss / (num_bucket)))
              running_loss = 0.0
 
         pbar.set_description(desc= f'Loss={loss.item()} Batch_id={idx} Accuracy={100*correct/processed:0.2f}')
