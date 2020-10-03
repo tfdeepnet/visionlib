ccbc4b25c5750ce0717c3193ebdee15bdb88333c
Deepak Hazarika <deepchin@users.noreply.github.com>
2020-10-03 11:50:20 +0530

    updated train and test file to store accuracy and loss

--- utils/test.py
+++ utils/test.py
@@ -6,18 +6,33 @@ import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
 
+test_losses = []
+test_acc = []
 
-def test(testloader, net, device ):
+def test(testloader, net, criterion , device ):
     correct = 0
     total = 0
+    test_loss = 0
+
+    net.eval()
     with torch.no_grad():
         for data in testloader:
             images, labels = data
             images, labels = images.to(device), labels.to(device)
             outputs = net(images)
+            test_loss += criterion(outputs, labels).item()  # sum up batch loss
             _, predicted = torch.max(outputs.data, 1)
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
-    print('Accuracy of the network on the 10000 test images: %d %%' % (
-            100 * correct / total))
+    testsetsize = len(testloader.dataset)
+    test_loss /= testsetsize
+    test_losses.append(test_loss)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+        test_loss, correct, testsetsize,
+        100. * correct / testsetsize))
+
+    test_acc.append(100. * correct / testsetsize)
+    return test_losses,test_acc
+
--- utils/train.py
+++ utils/train.py
@@ -6,12 +6,18 @@ import torch.optim
 import torch.utils.data
 import torchvision.transforms as transforms
 import torchvision.datasets as datasets
+from tqdm import tqdm
 
 
 def train(epoch,  trainloader, optimizer, net, criterion, num_bucket,device):
 
     running_loss = 0.0
-    for i, data in enumerate(trainloader, 0):
+    correct = 0
+    processed = 0
+
+    pbar = tqdm(trainloader)
+
+    for idx, data in enumerate(pbar) : #trainloader, 0):
         # get the inputs
         inputs, labels = data
         inputs, labels = inputs.to(device), labels.to(device)
@@ -25,10 +31,41 @@ def train(epoch,  trainloader, optimizer, net, criterion, num_bucket,device):
         loss.backward()
         optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
-        if i % (num_bucket) == 0:    # print every 2000 mini-batches
-            print('[%d, %5d] loss: %.3f' %
-                  (epoch + 1, i + 1, running_loss / (num_bucket)))
-            running_loss = 0.0
+        _, predicted = torch.max(outputs.data, 1)
+        processed += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
+        # print statistics
+        # running_loss += loss.item()
+        # if i % (num_bucket) == 0:    # print every 2000 mini-batches
+        #     print('[%d, %5d] loss: %.3f' %
+        #           (epoch + 1, i + 1, running_loss / (num_bucket)))
+        #     running_loss = 0.0
+
+        pbar.set_description(desc= f'Loss={loss.item()} Batch_id={idx} Accuracy={100*correct/processed:0.2f}')
+
+train_losses = []
+train_acc = []
+
+def train_metrics(trainloader, net, criterion , device ):
+    correct = 0
+    total = 0
+    train_loss = 0
+
+    net.eval()
+    with torch.no_grad():
+        for data in trainloader:
+            images, labels = data
+            images, labels = images.to(device), labels.to(device)
+            outputs = net(images)
+            train_loss += criterion(outputs, labels).item()  # sum up batch loss
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    trainsetsize = len(trainloader.dataset)
+    train_loss /= trainsetsize
+    train_losses.append(train_loss)
+
+    train_acc.append(100. * correct / trainsetsize)
+    return train_losses,train_acc
